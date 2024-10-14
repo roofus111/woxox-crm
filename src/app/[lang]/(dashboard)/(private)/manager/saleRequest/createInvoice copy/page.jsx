08dd1a66245c023ba55dabbc7f45182fbaec6859
Sooraj <soorajphotography1999@gmial.com>
2024-10-14 23:38:31 +0530

    missed file

--- src/app/[lang]/(dashboard)/(private)/manager/saleRequest/createInvoice copy/page.jsx
+++ src/app/[lang]/(dashboard)/(private)/manager/saleRequest/createInvoice copy/page.jsx
@@ -4,8 +4,7 @@ import { useSearchParams } from 'next/navigation'
 const CreateInvoice = () => {
     const searchParams = useSearchParams()
     const id = searchParams.get('id')
-
-
+    
     return (
         <h1>Create Invoice</h1>
     )

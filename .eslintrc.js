fa3567c2d9b0389972fb45e40c674b540cdbc5c9
Sooraj <soorajphotography1999@gmial.com>
2024-12-26 00:42:37 +0530

    eslint disabling

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,40 @@
 module.exports = {
-  "extends": [],
+  "extends": [
+    "next/core-web-vitals",
+    "plugin:import/recommended",
+    "prettier"
+  ],
   "rules": {
-    "react/jsx-key": "off",
-    "react-hooks/rules-of-hooks": "off",
-    "import/no-duplicates": "off",
-    "react/no-unescaped-entities": "off",
     "jsx-a11y/alt-text": "off",
     "react/display-name": "off",
+    "react/no-unescaped-entities": "off",
     "react/no-children-prop": "off",
+    "@next/next/no-img-element": "off",
+    "@next/next/no-page-custom-font": "off",
     "lines-around-comment": "off",
     "padding-line-between-statements": "off",
     "newline-before-return": "off",
     "import/newline-after-import": "off",
     "import/order": "off",
-    "react-hooks/exhaustive-deps": "off",
-    "@next/next/no-img-element": "off",
-    "@next/next/no-page-custom-font": "off"
+    // Disabling React Hook Rules
+    "react-hooks/exhaustive-deps": "off", // Disables missing dependencies warnings
+    "react-hooks/rules-of-hooks": "off",  // Disables React hook usage errors
+    // Disabling Import Duplicate Rules
+    "import/no-duplicates": "off", // Disables duplicate import warnings
+    // Disabling JSX Key Warnings
+    "react/jsx-key": "off" // Disables missing "key" prop warnings in iterators
   },
   "settings": {
     "react": {
       "version": "detect"
+    },
+    "import/parsers": {},
+    "import/resolver": {
+      "node": {},
+      "typescript": {
+        "project": "./jsconfig.json"
+      }
     }
-  }
+  },
+  "overrides": []
 }
